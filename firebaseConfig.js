ba77251469eaf559457706bcdb744d4bab4a01e9
sinclqir <sinclair.balivet@etu.unice.fr>
2024-04-12 09:56:26 +0200

    update f

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,19 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBJsCYE-zLpV4OvsV6vJ0KKWG1aTGbqTo0",
-    authDomain: "webcloudynov.firebaseapp.com",
-    projectId: "webcloudynov",
-    storageBucket: "webcloudynov.appspot.com",
-    messagingSenderId: "715930428973",
-    appId: "1:715930428973:web:6b2f4ac392d2f6d4fe7aa4",
-    measurementId: "G-88RM3J79TV"
+  apiKey: "AIzaSyCmz0XZX7YEq4iUE4oY3pm2uiWAh6XHboY",
+  authDomain: "web-cloud-ynov-46f1d.firebaseapp.com",
+  projectId: "web-cloud-ynov-46f1d",
+  storageBucket: "web-cloud-ynov-46f1d.appspot.com",
+  messagingSenderId: "197393246051",
+  appId: "1:197393246051:web:631a2a3a33dc61f1a05556",
+  measurementId: "G-7XVNWFNRP5"
 };
 
 // Initialize Firebase
